3859d55c9d19feff217e4b8822a967a7b784133c
porcel1910 <porcel_augusto@hotmail.com>
2021-09-27 19:31:37 -0300

    servidor node

--- Index.js
+++ Index.js
@@ -5,7 +5,33 @@ let server =  http.createServer((req, res)=>{
 console.log("URL", req.url)
 console.log("METHOD", req.method);
 
-res.end('Ok')
+switch (req.url){
+
+    case '/':
+    
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end ('<h1>Olá</h1> ');
+
+    break;
+
+    case '/users':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.end (JSON.stringify({
+            users:[{
+                name:'Jose augusto',
+                email:'porcel_augusto@hotmaol.com ',
+                id: 1
+            }]
+        }));
+
+    break;
+
+
+}
 
 });
 
