475a56aee5c61d4b754cca24068f1c0c273b8a09
porcel1910 <porcel_augusto@hotmail.com>
2021-09-30 19:49:51 -0300

    node 12

---
+++ utils/error.js
@@ -0,0 +1,15 @@
+module.exports ={
+
+    send:(err, req, res, code =400)=>{
+        console.log(`error: ${err}`);
+        res.status(code).json({
+            error:err
+
+        });
+
+
+    }
+
+
+
+};
