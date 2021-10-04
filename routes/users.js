c8bb8c63aec2251ff502d52ae19c2b4359f431d6
porcel1910 <porcel_augusto@hotmail.com>
2021-10-04 18:51:29 -0300

    node

--- routes/users.js
+++ routes/users.js
@@ -51,4 +51,59 @@ module.exports = app=> {
         });
     });
 
+    let routeId = app.route('/users/:id'); 
+    routeId.get((req, res)=>{
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+            
+            if(err){
+
+                app.utils.error.send(err, req, res );
+
+            } else{
+
+                res.status(200).json(user);
+
+            }
+
+
+        });
+
+    });
+
+    routeId.put((req, res)=>{
+        db.update({_id:req.params.id},req.body , err =>{
+            
+            if(err){
+
+                app.utils.error.send(err, req, res );
+
+            } else{
+
+                res.status(200).json(Object.assign(req.params, req.body));
+
+            }
+
+
+        });
+
+    });
+    routeId.delete((req, res)=>{
+
+        db.remove({_id:req.params.id}, {}, err=>{
+
+            if(err){
+
+                app.utils.error.send(err, req, res );
+
+            } else{
+
+                res.status(200).json(req.params);
+
+            }
+
+
+        });
+
+    });
+
   };
