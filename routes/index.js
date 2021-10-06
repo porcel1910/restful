e93bd58f30c56da7eaabb8a23be8a2525f02d888
porcel1910 <porcel_augusto@hotmail.com>
2021-10-06 19:59:16 -0300

    node

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,12 @@
 
-
-
-
-
-
-module.exports = app=>{
-
-
-    app.get('/',(req, res)=>{
+module.exports = app => {
+ 
+    app.get('/', (req, res) => {
 
         res.statusCode = 200;
-        res.setHeader('Content-Type','text/html');
-        res.end ('<h1>Ola Mundo</h1> '); 
-    
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>RESTful API</h1><p>Projeto desenvolvido no Curso Completo de JavaScript da Hcode na Udemy.com</p>');
+
     });
-
+    
 };
-
--- routes/users.js
+++ routes/users.js
@@ -1,121 +1,95 @@
 let NeDB = require('nedb');
 let db = new NeDB({
     filename:'users.db',
-    autoload : true
-
-    
+    autoload:true
 });
 
+module.exports = app => {
 
+    let route = app.route('/users');
 
-module.exports = app=> {
-
-    let route = app.route('/users'); 
-
-    route.get((req, res) =>{
+    route.get((req, res) => {
 
         db.find({}).sort({name:1}).exec((err, users)=>{
-            if(err){
-               app.utils.error.send(err, req, res );
 
-
-            }else{
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
 
                 res.statusCode = 200;
-                res.setHeader('Content-Type','application/json');
+                res.setHeader('Content-Type', 'application/json');
                 res.json({
                     users
-                }); 
-            }
+                });
 
+            }
 
         });
 
+    });
+
+    route.post((req, res) => {
         
+        if (!app.utils.validator.user(app, req, res)) return false;
+        
+        db.insert(req.body, (err, user)=>{
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+
+                res.status(200).json(user);
+
+            }
+
+        });
+
+    });
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
+
+        });
+
+    });
+
+    routeId.put((req, res) => {
+        
+        if (!app.utils.validator.user(app, req, res)) return false;
+
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+
+        });
+
     });
     
-    route.post((req, res)=>{
-
-        req.assert('name',' O nome é obrigatorio.').notEmpty();
-        req.assert('email',' O email está invalido.').notEmpty().isEmail();
-        let errors = req.validationErrors();
-
-        if(errors) {
-
-            app.utils.error.send(errors, req, res );
-            return false;
-
-
-        }
-
-        db.insert(req.body,(err, user)=>{
-
-            if(err){
-
-                app.utils.error.send(err, req, res );
-
-            } else{
-
-                res.status(200).json(user);
-
-            }
-
-        });
-    });
-
-    let routeId = app.route('/users/:id'); 
-    routeId.get((req, res)=>{
-        db.findOne({_id:req.params.id}).exec((err, user)=>{
-            
-            if(err){
-
-                app.utils.error.send(err, req, res );
-
-            } else{
-
-                res.status(200).json(user);
-
-            }
-
-
-        });
-
-    });
-
-    routeId.put((req, res)=>{
-        db.update({_id:req.params.id},req.body , err =>{
-            
-            if(err){
-
-                app.utils.error.send(err, req, res );
-
-            } else{
-
-                res.status(200).json(Object.assign(req.params, req.body));
-
-            }
-
-
-        });
-
-    });
     routeId.delete((req, res)=>{
 
-        db.remove({_id:req.params.id}, {}, err=>{
+        db.remove({ _id: req.params.id }, {}, err=>{
 
-            if(err){
-
-                app.utils.error.send(err, req, res );
-
-            } else{
-
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
                 res.status(200).json(req.params);
-
             }
-
 
         });
 
     });
 
-  };
+};
